b131f7671e31be213c9683d88bc8c4ac85931891
Dziumenko Heorhii <dzymenko.georgy@gmail.com>
2024-05-10 00:19:47 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 export default defineConfig({
     plugins: [react()],
     resolve: {
+        base: '/graph/',
         alias: {
             '@theme': resolve('src/theme'),
             '@features': resolve('src/features'),
